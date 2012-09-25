a21531906822a6b52553d42eed49e5aa5cf53be1
evx001 <eviexeris@yahoo.com>
2012-09-25 17:23:54 -0400

    CapPlayground version begun

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,30 +1,57 @@
-set :application, "samantha-cook.com"
-set :repository,  "git@github.com:evx001/sam_cms.git"
+set :user , "root"
+set :domain_password, Proc.new {CLI.password_prompt "desired domain password: "}
+set :database_password, Proc.new {CLI.password_prompt "desired database password: "}
+set :deploy_to, "/home/admin/#{application}"
+set :shared_directory, "#{deploy_to}/shared"
+set :use_sudo, false
+set :group_writable, false
+default_run_options[:pty] = true
 
-set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+role :app, ip_address
+role :web, ip_address
+role :db,  ip_address, :primary => true
 
-role :web, "samantha-cook.com"                          # Your HTTP server, Apache/etc
-role :app, "samantha-cook.com"                          # This may be the same as your `Web` server
-role :db,  "samantha-cook.com", :primary => true # This is where Rails migrations will run
-	
-	set :user, 'samantha'
-	set :use_sudo, false
-	set :deploy_to, /.../www/"#{app location}"
-	set :deploy_via, :remote_cache # this so it will only pull the changes from github
-
-
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-  namespace :deploy do
-    task :start do ; end
-    task :stop do ; end
-    task :restart, :roles => :app, :except => { :no_release => true } do
-      run "touch #{File.join(current_path,'tmp','restart.txt')}"
-    end 
+task :after_update_code, :roles => [:web, :db, :app] do
+  run "chmod 755 #{release_path}/public"
+  run "chown admin:admin #{release_path} -R"
+  begin
+    run "rm -f #{release_path}/config/database.yml"
+  rescue Exception => error
   end
+  run "ln -s #{shared_directory}/database.yml #{release_path}/config/database.yml"
+end
+
+namespace :deploy do
+  desc "restart passenger"
+  task :restart do
+    passenger::restart
+  end
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+
+end
+
+namespace :passenger do
+  desc "Restart passenger"
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
+
+task :setup_domain, :hosts => ip_address do
+  begin
+     run "/script/add --name=rpdefault --v-webipaddress=#{ip_address}  --v-mmailipaddress=#{ip_address} --class=dnstemplate --v-nameserver_f=ns1.vpsplayground.net --v-secnameserver_f=ns2.vpsplayground.net"
+  rescue Exception => error
+    puts "dns zone already added."
+  end
+  run "/script/add --class=domain --name=#{domain_name} --v-docroot=#{application}/current/public --v-password=#{domain_password} --v-dnstemplate_name=rpdefault.dnst"
+  run "/script/add --class=mysqldb --name=#{database_name} --v-dbpassword=#{database_password}"
+  database_config = "production:\n  adapter: mysql\n  encoding: utf8\n  database: #{database_name}\n  username: #{database_name}\n  password: #{database_password}"
+  puts database_config
+  put database_config, "#{shared_directory}/database.yml"
+  run "rm -rf /home/admin/#{application}/current"
+  puts "\n\nYou will now login to hypervm at https://vps.webhostserver.biz:8887, click on DNS and add zone record for #{domain_name}, then set your nameservers for your domain at your domain registrar to ns1.vpsplayground.net and ns2.vpsplayground.net\n\n"
+
+end
